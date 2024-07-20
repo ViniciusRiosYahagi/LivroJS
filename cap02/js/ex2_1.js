97329af033ea3ca3fb06d5e6e4fa0c1c6b349917
Vinicius Rios <viniciusrios.x3@gmail.com>
2024-07-20 17:20:40 -0300

    Exemplo 2.1

---
+++ cap02/js/ex2_1.js
@@ -0,0 +1,10 @@
+// Cria refência ao form e ao elemento h3 (onde será exibido a resposta)
+const frm = document.querySelector("form")
+const resp = document.querySelector("h3")
+
+// Cria um "ouvinte" de evento, acionado quando o botão submit for clicado.
+frm.addEventListener("submit", (e) => {
+  const name = frm.inName.value // Obtém o nome digitado no form.
+  resp.innerText = name // Exibe a resposta do programa.
+  e.preventDefault() // Evita envio do form
+})
